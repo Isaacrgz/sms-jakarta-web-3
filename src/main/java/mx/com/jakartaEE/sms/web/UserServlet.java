72940a9c884d177baea544974d286211ce8fb5da
Isaacrgz <isaacrgz97@gmail.com>
2023-11-07 22:34:38 -0600

    Fist commit to github

---
+++ src/main/java/mx/com/jakartaEE/sms/web/UserServlet.java
@@ -0,0 +1,24 @@
+package mx.com.jakartaEE.sms.web;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.util.List;
+import mx.com.jakartaEE.sms.domain.User;
+import mx.com.jakartaEE.sms.service.UserService;
+
+@WebServlet("/User")
+public class UserServlet extends HttpServlet{
+    @Inject
+    UserService userService;
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        List<User> users = userService.listUser();
+        System.out.println("Users:" + users);
+        request.setAttribute("users", users);
+        request.getRequestDispatcher("/listadoUsuarios.jsp").forward(request, response);
+    }
+}
